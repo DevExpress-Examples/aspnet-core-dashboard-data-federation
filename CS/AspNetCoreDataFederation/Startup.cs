9bebf9f46bf3cc6db5985fc72c89ce2f58057f5e
Polina Tyureva <polina.tyureva@devexpress.com>
2022-11-21 22:52:26 +0400

    Update (BC 1127730)

--- CS/AspNetCoreDataFederation/Startup.cs
+++ CS/AspNetCoreDataFederation/Startup.cs
@@ -108,7 +108,7 @@ namespace AspNetCoreDataFederation {
 
             app.UseRouting();
             app.UseEndpoints(endpoints => {
-                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard", "DefaultDashboard");
+                endpoints.MapDashboardRoute("api/dashboard", "DefaultDashboard");
                 endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
